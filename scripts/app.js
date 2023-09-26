a678eb17eb7481daf479eb6f89a3ec2f066bc13f
Anton Henningsson <anton_9408@hotmail.com>
2023-09-26 15:53:48 +0200

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,23 +1,31 @@
-require('dotenv').config();
-
+// require('dotenv').config();
 const { API_URL, PRIVATE_KEY, CONTRACT_ADDRESS } = process.env;
-
-const { ethers, network } = require('hardhat');
+const { ethers } = require('hardhat');
 const contract = require('../artifacts/contracts/DumbMessage.sol/DumbMessage.json');
-
-//const provider = new ethers.providers.AlchemyProvider(network= 'sepolia'), API_KEY); //provider 
-
+// Provider - Alchemy
+// const provider = new ethers.providers.AlchemyProvider('sepolia', API_KEY); Funkar inte längre
 const provider = new ethers.providers.JsonRpcProvider(API_URL);
-
-//signer = (owner, my wallet)
-
+// Signer - Jag
+const signer = new ethers.Wallet(PRIVATE_KEY, provider);
+// Kontraktet och skapa en ny instans...
 const dumbContract = new ethers.Contract(CONTRACT_ADDRESS, contract.abi, signer);
 
 async function init() {
-    const message = await dumbContract.message(); 
-    console.log('Current message is: ', message);
+  const message = await dumbContract.message();
+  console.log('Current message is: ', message);
 
+  console.log('Updating the message');
+  const tx = await dumbContract.SetMessage('This is a new message');
+  await tx.wait();
+
+  const updatedMessage = await dumbContract.message();
+  console.log('The new message is: ', updatedMessage);
 }
 
-init().then(() => proccess.exit(0)).catch((error) => {console.error(error); process.exit(1));
-}
+// funktionsanrop => går till raden 13 och kör funktionen init()
+init()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
